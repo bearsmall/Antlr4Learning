8ba312d16296b1c430e2b25e0b36c421729798f8
bearsmall <bearsmall@bupt.edu.cn>
2018-12-26 18:23:44 +0800

    'modify'

--- src/test/java/com/method/demo1/MethodExtractor.java
+++ src/test/java/com/method/demo1/MethodExtractor.java
@@ -1,4 +1,81 @@
-package com.method.demo;
+package com.method.demo1;
+
+import com.cmp.antlr.java.JavaLexer;
+import com.cmp.antlr.java.JavaParser;
+import com.method.demo1.listener.MethodExtractorListenner;
+import org.antlr.v4.runtime.*;
+import org.antlr.v4.runtime.tree.ParseTree;
+import org.antlr.v4.runtime.tree.ParseTreeWalker;
+import org.junit.Test;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class MethodExtractor {
+    @Test
+    public void test() throws IOException {
+        CharStream inputStream = CharStreams.fromStream(MethodExtractor.class.getClassLoader().getResourceAsStream("language\\java\\Method.java"));
+        JavaLexer lexer = new JavaLexer(inputStream);
+        CommonTokenStream tokenStream = new CommonTokenStream(lexer);
+        JavaParser parser = new JavaParser(tokenStream);
+        ParseTree tree = parser.methodDeclaration();  //运行我们的特殊版本的语法分析器【重写了defineProperty方法】
+
+        ParseTreeWalker walker = new ParseTreeWalker();//新建一个标准的ANTLR语法分析树遍历器
+        MethodExtractorListenner loader = new MethodExtractorListenner(inputStream.toString(),tokenStream.getTokens());//新建一个监听器，将其传递给遍历器
+        walker.walk(loader,tree);   //遍历语法分析器
+        StringBuffer str = new StringBuffer();
+        List<Token> tokenList = modifyTokenValue(loader);
+        for(int i=0;i<tokenList.size()-1;i++){
+            str.append(tokenList.get(i).getText());
+        }
+        System.out.println(str);
+        System.out.println(loader);//从map中取值
+    }
+
+    /**
+     * Token值类型替换
+     * @param loader
+     * @return
+     */
+    private List<Token> modifyTokenValue(MethodExtractorListenner loader) {
+        List<Token> formalParamInterval = loader.getFormalParamInterval();//形式参数
+        List<Token> localParamInterval = loader.getLocalParamInterval();//局部变量
+        List<Token> dataTypeInterval = loader.getDataTypeInterval();//数据类型
+        List<Token> methodCallInterval = loader.getMethodCallInterval();//方法调用
+        List<Token> paramInterval = loader.getParamInterval();//无标识参数
+        List<Token> tokenList = loader.getTokenList();//token列表
+
+        for(Token token: formalParamInterval){
+            for(Token t : paramInterval){
+                if(t.getText().equals(token.getText())) {
+                    tokenList.set(t.getTokenIndex(),new CommonToken(1, "FORMAL_PARAM"));
+
+                }
+            }
+            tokenList.set(token.getTokenIndex(),new CommonToken(1,"FORMAL_PARAM"));
+        }
+        for(Token token : localParamInterval){
+            for(Token t : paramInterval){
+                if(t.getText().equals(token.getText())) {
+                    tokenList.set(t.getTokenIndex(),new CommonToken(1, "LOCAL_PARAM"));
+                }
+            }
+            tokenList.set(token.getTokenIndex(),new CommonToken(1,"LOCAL_PARAM"));
+        }
+        for(Token token : dataTypeInterval){
+            tokenList.set(token.getTokenIndex(),new CommonToken(1,"DATA_TYPE"));
+        }
+        for(Token token : methodCallInterval){
+            tokenList.set(token.getTokenIndex(),new CommonToken(1,"METHOD_CALL"));
+        }
+        for(Token token : paramInterval){
+            String txt = tokenList.get(token.getTokenIndex()).getText();
+            if(txt.equals("FORMAL_PARAM")||txt.equals("LOCAL_PARAM")) {
+            }else {
+                tokenList.set(token.getTokenIndex(),new CommonToken(1, "GLOBAL_PARAM"));
+            }
+        }
+        return tokenList;
+    }
 }
--- src/test/java/com/method/demo1/listener/MethodExtractorListenner.java
+++ src/test/java/com/method/demo1/listener/MethodExtractorListenner.java
@@ -1,71 +1,98 @@
-package com.cmp.listener;
+package com.method.demo1.listener;
 
 import com.cmp.DefaultCodeFile;
 import com.cmp.ModuleEntity;
 import com.cmp.antlr.java.JavaParser;
 import com.cmp.antlr.java.JavaParserBaseListener;
 import org.antlr.v4.runtime.Token;
-import org.antlr.v4.runtime.TokenStream;
+import org.antlr.v4.runtime.misc.Interval;
+import org.antlr.v4.runtime.tree.ParseTree;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class MethodExtractorListenner extends JavaParserBaseListener {
 
-    private DefaultCodeFile defaultCodeFile;
-    private List<Token> tokenList;
+    private List<ModuleEntity> moduleEntityList = new ArrayList<>();
+    private List<Token> formalParamInterval = new ArrayList<>();
+    private List<Token> localParamInterval = new ArrayList<>();
+    private List<Token> dataTypeInterval = new ArrayList<>();
+    private List<Token> methodCallInterval = new ArrayList<>();
 
-    public MethodExtractorListenner(DefaultCodeFile defaultCodeFile, List<Token> tokenList) {
-        this.defaultCodeFile = defaultCodeFile;
+    private List<Token> paramInterval = new ArrayList<>();
+
+    private List<Token> tokenList;
+    private String content;
+
+    public List<ModuleEntity> getModuleEntityList() {
+        return moduleEntityList;
+    }
+
+    public void setModuleEntityList(List<ModuleEntity> moduleEntityList) {
+        this.moduleEntityList = moduleEntityList;
+    }
+
+    public List<Token> getTokenList() {
+        return tokenList;
+    }
+
+    public void setTokenList(List<Token> tokenList) {
         this.tokenList = tokenList;
     }
 
-    /**
-     * 根据起始行号截取起止文本
-     * @param start
-     * @param stop
-     * @return
-     */
-    private String getPureContentWithLine(Token start, Token stop) {
-        int startLine = start.getLine();
-        int stopLine = stop.getLine();
-        Pattern pattern = Pattern.compile("\n");
-        Matcher find = pattern.matcher(defaultCodeFile.getContent());
-        int number = 0;
-        int i = 0;
-        if (startLine != 1) {
-            while (find.find()) {
-                number++;
-                if (number == startLine - 1) {
-                    break;
-                }
-            }
-            i = find.start() + 1;
-        }
-        int j = defaultCodeFile.getContent().length();
-        if (stopLine != defaultCodeFile.getLineNum()) {
-            while (find.find()) {
-                number++;
-                if (number == stopLine) {
-                    break;
-                }
-            }
-            j = find.start();
-        }
-        return defaultCodeFile.getContent().substring(i, j);
+    public String getContent() {
+        return content;
     }
 
-    /**
-     * 根据字符串起始偏移量截取起止文本
-     * @param start
-     * @param stop
-     * @return
-     */
-    private String getPureContent(Token start, Token stop) {
-        int starOff = start.getStartIndex();
-        int stopOff = stop.getStopIndex();
-        return defaultCodeFile.getContent().substring(starOff, stopOff+1);
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public List<Token> getFormalParamInterval() {
+        return formalParamInterval;
+    }
+
+    public void setFormalParamInterval(List<Token> formalParamInterval) {
+        this.formalParamInterval = formalParamInterval;
+    }
+
+    public List<Token> getLocalParamInterval() {
+        return localParamInterval;
+    }
+
+    public void setLocalParamInterval(List<Token> localParamInterval) {
+        this.localParamInterval = localParamInterval;
+    }
+
+    public List<Token> getDataTypeInterval() {
+        return dataTypeInterval;
+    }
+
+    public void setDataTypeInterval(List<Token> dataTypeInterval) {
+        this.dataTypeInterval = dataTypeInterval;
+    }
+
+    public List<Token> getMethodCallInterval() {
+        return methodCallInterval;
+    }
+
+    public void setMethodCallInterval(List<Token> methodCallInterval) {
+        this.methodCallInterval = methodCallInterval;
+    }
+
+    public List<Token> getParamInterval() {
+        return paramInterval;
+    }
+
+    public void setParamInterval(List<Token> paramInterval) {
+        this.paramInterval = paramInterval;
+    }
+
+    public MethodExtractorListenner(String content, List<Token> tokenList) {
+        this.content = content;
+        this.tokenList = tokenList;
     }
 
     private ModuleEntity extractModuleEntity(Token start,Token stop) {
@@ -73,57 +100,72 @@ public class MethodExtractorListenner extends JavaParserBaseListener {
         int charEnd = stop.getStartIndex();
         int tokenStart = start.getTokenIndex();
         int tokenEnd = stop.getTokenIndex();
-        return new ModuleEntity(charStart,charEnd,tokenStart,tokenEnd,defaultCodeFile.getContent().substring(charStart,charEnd+1),tokenList.subList(tokenStart,tokenEnd));
+        return new ModuleEntity(charStart,charEnd,tokenStart,tokenEnd,content.substring(charStart,charEnd+1),tokenList.subList(tokenStart,tokenEnd));
     }
 
     @Override
     public void enterMethodDeclaration(JavaParser.MethodDeclarationContext ctx) {
 //        String method = getPureContent(ctx.getStart(), ctx.getStop());
         ModuleEntity moduleEntity = extractModuleEntity(ctx.getStart(),ctx.getStop());
-        defaultCodeFile.getMethods().add(moduleEntity);
+        moduleEntityList.add(moduleEntity);
+    }
+
+
+
+    @Override
+    public void enterFormalParameter(JavaParser.FormalParameterContext ctx) {
+        ParseTree parseTree = ctx.getChild(1);
+        String param = parseTree.getText();
+        Interval interval = parseTree.getSourceInterval();
+        System.out.println("enterFormalParameter-->"+param);
+        formalParamInterval.add(tokenList.get(interval.a));
     }
 
     @Override
-    public void enterConstructorDeclaration(JavaParser.ConstructorDeclarationContext ctx) {
-//        String construction = getPureContent(ctx.getStart(), ctx.getStop());
-        ModuleEntity moduleEntity = extractModuleEntity(ctx.getStart(),ctx.getStop());
-        defaultCodeFile.getConstructor().add(moduleEntity);
-    }
-
-    @Override
-    public void enterClassDeclaration(JavaParser.ClassDeclarationContext ctx) {
-//        String classBlock = getPureContent(ctx.getStart(), ctx.getStop());
-        ModuleEntity moduleEntity = extractModuleEntity(ctx.getStart(),ctx.getStop());
-        defaultCodeFile.getClassBlocks().add(moduleEntity);
-    }
-
-    @Override
-    public void enterInterfaceDeclaration(JavaParser.InterfaceDeclarationContext ctx) {
-//        String interfaceBlock = getPureContent(ctx.getStart(), ctx.getStop());
-        ModuleEntity moduleEntity = extractModuleEntity(ctx.getStart(),ctx.getStop());
-        defaultCodeFile.getInterfaceBlocks().add(moduleEntity);
-    }
-
-    @Override
-    public void enterEnumDeclaration(JavaParser.EnumDeclarationContext ctx) {
-//        String enumBlock = getPureContent(ctx.getStart(), ctx.getStop());
-        ModuleEntity moduleEntity = extractModuleEntity(ctx.getStart(),ctx.getStop());
-        defaultCodeFile.getEnumBlocks().add(moduleEntity);
-    }
-
-    @Override
-    public void enterBlock(JavaParser.BlockContext ctx) {
-        if(ctx.getParent().getRuleIndex()==JavaParser.RULE_classBodyDeclaration) {
-//            String staticBlock = getPureContent(ctx.getStart(), ctx.getStop());
-            ModuleEntity moduleEntity = extractModuleEntity(ctx.getStart(),ctx.getStop());
-            defaultCodeFile.getStaticBlocks().add(moduleEntity);
+    public void enterLocalVariableDeclaration(JavaParser.LocalVariableDeclarationContext ctx) {
+        ParseTree parseTree = ctx.getChild(1);
+        for(int i=0;i<parseTree.getChildCount();i++){
+            ParseTree children = parseTree.getChild(i).getChild(0);
+            String param = children.getText();
+            Interval interval = children.getSourceInterval();
+            System.out.println("enterLocalVariableDeclaration-->"+param);
+            localParamInterval.add(tokenList.get(interval.a));
         }
     }
 
     @Override
-    public void enterFieldDeclaration(JavaParser.FieldDeclarationContext ctx) {
-//        String field = getPureContent(ctx.getStart(), ctx.getStop());
-        ModuleEntity moduleEntity = extractModuleEntity(ctx.getStart(),ctx.getStop());
-        defaultCodeFile.getFields().add(moduleEntity);
+    public void enterClassOrInterfaceType(JavaParser.ClassOrInterfaceTypeContext ctx) {
+        String param = ctx.getText();
+        Interval interval = ctx.getSourceInterval();
+        System.out.println("enterClassOrInterfaceType-->"+param);
+        dataTypeInterval.add(tokenList.get(interval.a));
+    }
+
+    @Override
+    public void enterPrimitiveType(JavaParser.PrimitiveTypeContext ctx) {
+        String param = ctx.getText();
+        Interval interval = ctx.getSourceInterval();
+        System.out.println("enterPrimitiveType-->"+param);
+        dataTypeInterval.add(tokenList.get(interval.a));
+    }
+
+    @Override
+    public void enterMethodCall(JavaParser.MethodCallContext ctx) {
+        ParseTree parseTree = ctx.getChild(0);
+        String param = parseTree.getText();
+        Interval interval = parseTree.getSourceInterval();
+        System.out.println("enterMethodCall-->"+param);
+        methodCallInterval.add(tokenList.get(interval.a));
+    }
+
+    @Override
+    public void enterPrimary(JavaParser.PrimaryContext ctx) {
+        if(ctx.getChild(0).getChildCount()>0){
+            return;
+        }
+        String param = ctx.getText();
+        Interval interval = ctx.getSourceInterval();
+        System.out.println("enterPrimary-->"+param);
+        paramInterval.add(tokenList.get(interval.a));
     }
 }
